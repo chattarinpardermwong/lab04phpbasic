ccfc3521b4d4eb69f19b86a6eda243d309c42e24
chattarinpardermwong <43002685+chattarinpardermwong@users.noreply.github.com>
2019-02-24 02:34:25 +0700

    Delete downloadbutton.php

--- lab04/downloadbutton.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-header('Content-Type: application/octet-stream');
-header("Content-Transfer-Encoding: Binary"); 
-header("Content-disposition: attachment; filename=\"" . "test.csv" . "\""); 
-readfile($test.csv);
-
-?>
